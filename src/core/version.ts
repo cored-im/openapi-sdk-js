0c382e6891fc68bb636bce75f22e5a6d42fee2e0
xalanq <xalanq@gmail.com>
2026-03-30 14:54:28 +0800

    release: Release 0.28.104

--- src/core/version.ts
+++ src/core/version.ts
@@ -1,5 +1,5 @@
 // Copyright (c) 2026 Cored Limited
 // SPDX-License-Identifier: Apache-2.0
 
-export const VERSION = '0.28.103';
-export const USER_AGENT = 'cored-openapi-sdk-js/0.28.103';
+export const VERSION = '0.28.104';
+export const USER_AGENT = 'cored-openapi-sdk-js/0.28.104';
